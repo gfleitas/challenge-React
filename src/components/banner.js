8b7f8235509905a88b5cb9a01864ced758a12ace
gfleitas <gabriel.fleiitas@gmail.com>
2019-10-10 12:38:03 -0300

    Terminado

--- src/components/banner.js
+++ src/components/banner.js
@@ -5,12 +5,12 @@ export default class Banner extends Component {
         return(
                 <div class="jumbotron jumbotron-fluid">
                     <div class="container text-center">
-                    <h1>¿que deseas aprender?</h1>
-                    <p>Aprende sobre programación desde cero, diseño UI/UX y mucho mas con Knowledge DB</p>
-                    <i class="fas fa-search"></i>
-                    <input class="mt-4" type="text" placeholder="¿Que deseas aprender hoy?"></input>
-                    <br></br>
-                    <button class="mt-5 btn-lg">Buscar recursos</button>
+                        <h1>¿que deseas aprender?</h1>
+                        <p>Aprende sobre programación desde cero, diseño UI/UX y mucho mas con Knowledge DB</p>
+                        <i class="fas fa-search"></i>
+                        <input class="mt-4" type="text" placeholder="¿Que deseas aprender hoy?"></input>
+                        <br></br>
+                        <button class="mt-5 btn-lg">Buscar recursos</button>
                     </div>
                 </div>
          );
